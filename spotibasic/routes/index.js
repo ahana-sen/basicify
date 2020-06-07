899b635f7c7ef5be758e8a4bae71b1c32662b7ee
Ahana Sen <as896@duke.edu>
2020-06-07 14:53:00 -0700

    ljk added a landing page

--- spotibasic/routes/index.js
+++ spotibasic/routes/index.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+let landing = require('../controllers/landing')
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', landing.get_landing);
 
 module.exports = router;
